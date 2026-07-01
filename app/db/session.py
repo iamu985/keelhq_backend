d9d0f229a31ad6fbe62aafa3754f8fcd046aec97
iamu985 <frankbr985gomes@gmail.com>
2026-07-01 21:12:08 +0530

    setup(db): add session factory

---
+++ app/db/session.py
@@ -0,0 +1,11 @@
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker
+from app.db.engine import ENGINE
+
+
+#  Create application session factory
+SessionLocal = async_sessionmaker(
+    bind=ENGINE,
+    class_=AsyncSession,
+    expire_on_commit=False,
+    autoflush=False,
+)
