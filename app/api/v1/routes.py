79b60b2d5e19597a8f26fdfcaf07fddba80f6843
iamu985 <frankbr985gomes@gmail.com>
2026-07-12 20:25:35 +0530

    fix(api/health): small fix for internal server error in health endpoint

--- app/api/v1/routes.py
+++ app/api/v1/routes.py
@@ -1,6 +1,7 @@
+import json
+
 from fastapi import APIRouter
 from fastapi.responses import JSONResponse
-from pydantic import SecretStr
 
 from app.core.config import settings
 
@@ -9,7 +10,8 @@ router = APIRouter()
 
 @router.get("/health")
 async def health_check() -> JSONResponse:
-    response = settings.model_dump()
-    password: SecretStr = response["database"]["password"]
-    response["database"]["password"] = password.get_secret_value()
-    return JSONResponse(status_code=200, content=response)
+    response = settings.model_dump_json()
+    # password: SecretStr = response["database"]["password"]
+    # response["database"]["password"] = password.get_secret_value()
+    config = json.loads(response)
+    return JSONResponse(status_code=200, content=config)
